5b3d809d4dad4328104d6b23602dffcdf459cabb
Adrien Thebo <adrien@puppetlabs.com>
2011-10-19 12:11:15 -0700

    Added simple file to dump a token for reuse.

---
+++ bin/gen_token.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'gdata'
+require 'yaml'
+
+token_file = "token.yaml"
+
+print "Username: "
+user = $stdin.gets.chomp
+
+print "Password: "
+%x{stty -echo}
+pass = $stdin.gets.chomp
+%x{stty echo}
+puts
+print "Service: "
+service = $stdin.gets.chomp
+
+auth = GData::Auth::ClientLogin.new(user, pass, service)
+token = auth.token
+
+if token.nil?
+  $stderr.puts "Authentication failed"
+  exit
+end
+
+token_hash = { :token => token, :created => Time.now }
+
+File.open(token_file, "w") do |f|
+  f.write(YAML.dump(token_hash))
+end
+
+puts "Token information written to #{token_file}"
