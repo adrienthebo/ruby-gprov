80170425d3b19627b3ac771e1c00eb33c77f1df4
Adrien Thebo <adrien@puppetlabs.com>
2011-12-03 01:32:00 -0800

    Collapsed exception code.

--- lib/gdata/error.rb
+++ lib/gdata/error.rb
@@ -1,5 +1,7 @@
 module GData
   module Error
+    class TokenInvalid < Exception; end
+    class InputInvalid < Exception; end
+    class QuotaExceeded < Exception; end
   end
 end
-require 'gdata/error/tokeninvalid'
--- lib/gdata/error/tokeninvalid.rb
+++
@@ -1,7 +0,0 @@
-module GData
-  module Error
-    class TokenInvalid < Exception
-    end
-  end
-end
-
