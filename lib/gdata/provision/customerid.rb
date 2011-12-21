2b66148f47bd13758f6e089ed4724b69cf84c31d
Adrien Thebo <adrien@puppetlabs.com>
2011-12-21 12:56:57 -0800

    Cleanup to customerid xmlattrs

--- lib/gdata/provision/customerid.rb
+++ lib/gdata/provision/customerid.rb
@@ -18,17 +18,22 @@ module GData
   module Provision
     class CustomerID < GData::Provision::EntryBase
 
-      xmlattr :customer_id, :xpath => %Q{property[@name = "customerId"]/@value}
-      xmlattr :name,        :xpath => %Q{property[@name = "name"]/@value}
-      xmlattr :description, :xpath => %Q{property[@name = "description"]/@value}
+      xmlattr :customer_id, :xpath => %Q{apps:property[@name = "customerId"]/@value}
+      xmlattr :name,        :xpath => %Q{apps:property[@name = "name"]/@value}
+      xmlattr :description, :xpath => %Q{apps:property[@name = "description"]/@value}
 
-      xmlattr :customer_org_unit_name, :xpath => %Q{property[@name = "customerOrgUnitName"]/@value}
-      xmlattr :customer_org_unit_description, :xpath => %Q{property[@name = "customerOrgUnitDescription"]/@value}
+      xmlattr :customer_org_unit_name do
+        xpath %Q{apps:property[@name = "customerOrgUnitName"]/@value}
+      end
+
+      xmlattr :customer_org_unit_description do
+       xpath %Q{apps:property[@name = "customerOrgUnitDescription"]/@value}
+      end
+
       def self.get(connection, options={})
         response = connection.get("/customer/2.0/customerId")
 
         document = Nokogiri::XML(response.body)
-        document.remove_namespaces!
         xml = document.root
 
         new(:status => :clean, :connection => connection, :source => xml)
