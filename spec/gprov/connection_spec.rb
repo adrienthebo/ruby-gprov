e65bc7fe6a96bd162e0f896e67ea00485ffa5a50
Adrien Thebo <adrien@puppetlabs.com>
2012-02-11 20:26:30 -0800

    clean up connection_spec

--- spec/gprov/connection_spec.rb
+++ spec/gprov/connection_spec.rb
@@ -2,58 +2,57 @@ require 'spec_helper'
 
 describe GProv::Connection do
 
-  before :each do
-    @klass = GProv::Connection
-    @instance = @klass.new("domain", "token")
+  let(:klass) { GProv::Connection }
 
-    @expected_options = {:headers => {
+  subject { GProv::Connection.new("domain", "token") }
+
+  let(:expected_options) do
+    {:headers => {
       'Authorization' => "GoogleLogin auth=token",
       'Content-Type' => 'application/atom+xml',
     }}
   end
 
   it "should use to google apps API url as the base uri" do
-    @klass.base_uri.should == "https://apps-apis.google.com/a/feeds"
+    klass.base_uri.should == "https://apps-apis.google.com/a/feeds"
   end
 
-  it "should have a domain accessor" do
-    @instance.respond_to?(:domain).should == true
-  end
+  it { should respond_to :domain }
 
   it "should expose the default headers" do
-    @instance.default_headers.should == @expected_options
+    subject.default_headers.should == expected_options
   end
 
 
   describe "http instance method" do
     [:put, :get, :post, :delete].each do |verb|
-      describe %Q{"#{verb}"} do
+
+      it { should respond_to verb }
+
+      describe "##{verb}" do
         before do
-          xml = %Q{<?xml version="1.0" encoding="UTF-8"?>\n<test xml="pointy"/>}
+          xml = %Q{<?xml version="1.0" encoding="UTF-8"?>\n<test xml="pointy" />}
           @stub_request = mock
           @stub_request.stubs(:code).returns 200
           @stub_request.stubs(:success?).returns true
           @stub_request.stubs(:class).returns HTTParty::Response
         end
 
-        it "should be an instance method" do
-          @instance.respond_to?(verb).should == true
-        end
 
         it "should be forwarded to the class" do
-          @klass.expects(verb).returns @stub_request
-          @instance.send(verb, '')
+          klass.expects(verb).returns @stub_request
+          subject.send(verb, '')
         end
 
         it "should return the http response" do
-          @klass.expects(verb).returns @stub_request
-          output = @instance.send(verb, "/url")
+          klass.expects(verb).returns @stub_request
+          output = subject.send(verb, "/url")
           output.class.should == HTTParty::Response
         end
 
         it "should interpolate the :domain substring" do
-          @klass.expects(verb).with("/domain", @expected_options).returns @stub_request
-          @instance.send(verb, "/:domain")
+          klass.expects(verb).with("/domain", expected_options).returns @stub_request
+          subject.send(verb, "/:domain")
         end
       end
     end
