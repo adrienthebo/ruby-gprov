50cbd0ae0cf8df340854c091ab96bd1d21803905
Adrien Thebo <adrien@puppetlabs.com>
2012-05-26 18:41:58 -0700

    Add comments about raised exceptions

--- lib/gprov/error.rb
+++ lib/gprov/error.rb
@@ -20,8 +20,15 @@ module GProv
     def initialize(request = nil)
       @request = request
     end
+
+    # Raised when the requesting user is not authenticated, IE has an invalid
+    # token
     class TokenInvalid < GProv::Error; end
+
+    # Raised when a request is malformed
     class InputInvalid < GProv::Error; end
+
+    # Raised when the Google Apps request quota is exceeded
     class QuotaExceeded < GProv::Error; end
   end
 end
