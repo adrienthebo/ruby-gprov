d3d177c7c6870d4dbfd378e09aa85b5e2dcea61b
Adrien Thebo <adrien@puppetlabs.com>
2012-05-26 18:09:26 -0700

    Fix entrybase specs

--- spec/gprov/provision/entrybase_spec.rb
+++ spec/gprov/provision/entrybase_spec.rb
@@ -5,7 +5,21 @@ require 'fakeentry'
 
 describe GProv::Provision::EntryBase do
 
-  [:status, :connection].each do |method|
-    it { should respond_to method }
+  let(:klass) { GProv::Provision::EntryBase }
+
+  describe "initialization" do
+
+    it "should require a connection" do
+      expect { klass.new }.to raise_error ArgumentError, /requires a connection parameter/
+    end
+  end
+
+  describe "basic methods" do
+    let(:connection) { stub 'connection' }
+    subject { klass.new(:connection => connection) }
+
+    [:status, :connection].each do |method|
+      it { should respond_to method }
+    end
   end
 end
